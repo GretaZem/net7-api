b1dc999643d706c1f0eb9247e855510c8b4b76f3
GretaZem <zemgulytegreta@gmail.com>
2023-09-27 15:56:23 +0300

    Add service to get external data

---
+++ net7-api/Services/ExternalApiService.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace net7_api.Services
+{
+    public class ExternalApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ExternalApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<PollutionPoint>> GetDataFromExternalApiAsync()
+        {
+            var response = await _httpClient.GetAsync("https://get.data.gov.lt/datasets/gov/lgt/potencialus_tarsos_zidiniai/TarsosZidinys/:format/json");
+            response.EnsureSuccessStatusCode();
+
+            var dataRoot = JsonConvert.DeserializeObject<DataRoot>(await response.Content.ReadAsStringAsync());
+
+            return dataRoot.Data;
+        }
+    }
+}
