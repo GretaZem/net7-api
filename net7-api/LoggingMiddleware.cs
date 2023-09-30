16629ab30b985cc83f26575ffe6507408ea9cd18
GretaZem <zemgulytegreta@gmail.com>
2023-09-30 17:41:29 +0300

    Add logging Middleware

---
+++ net7-api/LoggingMiddleware.cs
@@ -0,0 +1,30 @@
+﻿namespace net7_api
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public LoggingMiddleware(RequestDelegate next, ILogger<LoggingMiddleware> logger)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            finally
+            {
+                _logger.LogInformation(
+                    "Request {method} {url} => {statusCode}",
+                    context.Request?.Method,
+                    context.Request?.Path.Value,
+                    context.Response?.StatusCode);
+            }
+        }
+    }
+}
